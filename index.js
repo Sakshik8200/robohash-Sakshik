dbbc610d875043a4f129a2e6a1b702f54156a6e1
Sakshi Khandelwal <khandelwalsakshi8200@gmail.com>
2021-10-03 14:31:54 +0530

    robohash used asynccwait

--- index.js
+++ index.js
@@ -11,28 +11,84 @@ function generateString(length) {
   return result;
 }
 console.log(generateString(8));
-//promise
+//async-wait
 function writeFilePromise(fileLocation, result) {
-        return new Promise((resolve, reject) => {
-            fs.writeFile(fileLocation, result, (err) => {
-                if (err) {
-                    reject('not able to write to the file')
-                }
-                resolve()
-            })
-        })
-    }
-    superagent.get(`https://robohash.org/${generateString(8)}`)
-        .end((err, res) => {
-            console.log(res.request.url);
-            return writeFilePromise('./robotImage.txt', res.request.url)
-        .then(() => {
-            console.log('random image is saved in file')
-        })
-        .catch((err) => {
-            console.log(err)
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileLocation, result, (err) => {
+            if (err) {
+                reject('not able to write to the file')
+            }
+            resolve()
         })
     })
+}
+async function getRobopic() {
+    try {
+        const res = await superagent.get(`https://robohash.org/${generateString(8)}`)
+        console.log('Random image is ', res.request.url)
+        await writeFilePromise('./robotImage.txt', res.request.url)
+        console.log('sucessfully written the file')
+    } catch (err) {
+        throw err
+    }
+    console.log('2. complete')
+}
+console.log('1. start')
+    ; (async () => {
+        try {
+            await getRobopic()
+            console.log('3. end')
+        } catch (err) {
+            console.log('3. end due to error')
+        }
+    })()
+
+// async function getRobopic(){
+//     try{
+//         const res = await superagent.get(
+//             `https://robohash.org/${generateString(8)}`
+//             )
+//         console.log('roboimg:',res.request.url);
+//         await writeFilePromise('/robotImage.txt',res.request.url)
+//         console.log('successfully added img')
+//         }catch(err){
+//             throw err
+//         }
+//         console.log('2.complete')
+
+// }
+// console.log('1.start')
+// ;(async()=>{
+//     try{
+//         await getRobopic()
+//         console.log('3. saved')
+
+//     }catch(err){
+//         console.log('3. error')
+//     }
+// })()
+// promise
+// function writeFilePromise(fileLocation, result) {
+//        return new Promise((resolve, reject) => {
+//             fs.writeFile(fileLocation, result, (err) => {
+//                 if (err) {
+//                    reject('not able to write to the file')
+//                  }
+//                  resolve()
+//              })
+//         })
+//     }
+//     superagent.get(`https://robohash.org/${generateString(8)}`)
+//         .end((err, res) => {
+//             console.log(res.request.url);
+//             return writeFilePromise('./robotImage.txt', res.request.url)
+//         .then(() => {
+//             console.log('random image is saved in file')
+//         })
+//         .catch((err) => {
+//             console.log(err)
+//         })
+//     })
 // callback
 
 // superagent

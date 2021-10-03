56aa2d057087ebe1780a39496dcff0d3ce14b13b
Sakshi Khandelwal <khandelwalsakshi8200@gmail.com>
2021-10-03 14:02:37 +0530

    robo hash added promise

--- index.js
+++ index.js
@@ -11,18 +11,41 @@ function generateString(length) {
   return result;
 }
 console.log(generateString(8));
-//callback
+//promise
+function writeFilePromise(fileLocation, result) {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(fileLocation, result, (err) => {
+                if (err) {
+                    reject('not able to write to the file')
+                }
+                resolve()
+            })
+        })
+    }
+    superagent.get(`https://robohash.org/${generateString(8)}`)
+        .end((err, res) => {
+            console.log(res.request.url);
+            return writeFilePromise('./robotImage.txt', res.request.url)
+        .then(() => {
+            console.log('random image is saved in file')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    })
+// callback
 
-superagent
-    .get(`https://robohash.org/${generateString(8)}`)
-    .end((err,res)=>{
-        console.log(res.request.url);
-        fs.writeFile('./robotImage.txt', res.request.url, (err) => {
-        if(err){
-            console.log("not able to save the file",err);
-            return;
-        }
-        console.log(`random img successfully savved in file.`)
-      });
-    });
+// superagent
+//     .get(`https://robohash.org/${generateString(8)}`)
+//     .end((err,res)=>{
+//         console.log(res.request.url);
+//         fs.writeFile('./robotImage.txt', res.request.url, (err) => {
+//         if(err){
+//             console.log("not able to save the file",err);
+//             return;
+//         }
+//         console.log(`random img successfully savved in file.`)
+//       });
+//     });
+
 
